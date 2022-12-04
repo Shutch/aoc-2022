33e990e09b12f2f6218ebdb16a63d9adfd65fcd0
Scott Hutchinson <shutchinson1190@gmail.com>
2022-12-04 13:30:28 +1000

    Adds gitignore, flake files, and update README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advent of Code 2022
 
-Written in Golang.
+Written in Golang (1.19.3).
+
+Also a test of github copilot with VSCode goodies.
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Demonstrating The Pipelines Pattern In Golang";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.bashInteractive
+            pkgs.go
+            pkgs.gotools
+            pkgs.golangci-lint
+            pkgs.gopls
+            pkgs.go-outline
+            pkgs.gopkgs
+            pkgs.delve
+          ];
+        };
+      });
+}
